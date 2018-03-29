40c0f601ec97b73522309295757ecc5e74cb65d7
TalhaTZA <talhavjtza@gmail.com>
2018-03-29 19:32:59 +0500

    ADD post to messages and ADD body parser library

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express=require('express');
-
+const bodyParser=require('body-parser');
 const app=express();
 
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var messages=[{
+    name:'Talha',message:'Hello'
+},{
+    name:'Ahmed',message:'Hi'
+}];
 
 app.get('/messages',(req,res,next)=>{
-    res.send([{
-        name:'Talha',message:'Hello'
-    },{
-        name:'Ahmed',message:'Hi'
-    }]);
+    res.send(messages);
+});
+
+app.post('/messages',(req,res,next)=>{
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 const server=app.listen(3000,()=>{
