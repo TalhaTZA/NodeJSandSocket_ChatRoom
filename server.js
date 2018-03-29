b93111983292e0645e491b8a34ea4018e2d1c2af
TalhaTZA <talhavjtza@gmail.com>
2018-03-29 14:43:02 +0500

    ADD page to serve index.html and ADD UI code for sending message

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express=require('express');
 
 const app=express();
 
+
+app.use(express.static(__dirname));
+
+
 const server=app.listen(3000,()=>{
     console.log('Listening on port '+server.address().port);
 });
-
