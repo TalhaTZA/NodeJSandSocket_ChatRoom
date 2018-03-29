f2237613b771f349b91b49833e577cf265419d74
TalhaTZA <talhavjtza@gmail.com>
2018-03-29 15:28:08 +0500

    ADD dummy functionality using ajax and FIX cdn for j query

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app=express();
 
 app.use(express.static(__dirname));
 
+app.get('/messages',(req,res,next)=>{
+    res.send([{
+        name:'Talha',message:'Hello'
+    },{
+        name:'Ahmed',message:'Hi'
+    }]);
+});
 
 const server=app.listen(3000,()=>{
     console.log('Listening on port '+server.address().port);
