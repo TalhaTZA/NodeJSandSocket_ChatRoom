e53f203cf321bc807280661f6b62f8f9561480e4
TalhaTZA <talhavjtza@gmail.com>
2018-04-02 20:07:24 +0500

    ADD changes for heroku

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ mongoose.connect(dbUrl,(err)=>{
     console.log('Connection Successful')
 });
 
-const server=http.listen(3000,()=>{
+const server=http.listen(process.env.PORT ||3000,()=>{
     console.log('Listening on port '+server.address().port);
 });
